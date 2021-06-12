22797a44f93c421b1546e89a8ef006ce776ac516
abdullahalharun <harun750@gmail.com>
2021-06-12 10:31:40 +0600

    update contact form & list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactCard from "./components/ContactCard";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Harun",
+      email: "harun@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Mamun",
+      email: "mamun@gmail.com"
+    },
+    {
+      id: "3",
+      name: "Adiba",
+      email: "adiba@gmail.com"
+    },
+  ]
+
   return (
-    <div>Hello World.</div>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
   );
 }
 
